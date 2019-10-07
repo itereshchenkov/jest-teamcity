07c9679128044af87b18606bf25cf28e617cbe81
Ivan Tereshchenkov <ivan.tereshchenkov@collect.ai>
2019-10-07 14:36:41 +0200

    [fix] Small updates
    * fix formatting
    * fix tests

--- __tests__/formatter.js
+++ __tests__/formatter.js
@@ -1,133 +1,133 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const formatter = require('../lib/formatter');
-const testData = require('./data');
+const path = require("path");
+const formatter = require("../lib/formatter");
+const testData = require("./data");
 const consoleOutput = [
-    ["##teamcity[testSuiteStarted name=\'foo/__tests__/file.js\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'path\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'to\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'test1\' flowId=\'12345\']"],
-    ["##teamcity[testStarted name=\'title1\' flowId=\'12345\']"],
-    ["##teamcity[testFailed name=\'title1\' flowId=\'12345\']"],
-    ["##teamcity[testFinished name=\'title1\' duration=\'123\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'test1\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'test2\' flowId=\'12345\']"],
-    ["##teamcity[testStarted name=\'title2\' flowId=\'12345\']"],
-    ["##teamcity[testIgnored name=\'title2\' message=\'pending\' flowId=\'12345\']"],
-    ["##teamcity[testFinished name=\'title2\' duration=\'123\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'test2\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'to\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'path\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'foo/__tests__/file.js\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'foo/__tests__/file2.js\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'path2\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'to\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'test3\' flowId=\'12345\']"],
-    ["##teamcity[testStarted name=\'title3\' flowId=\'12345\']"],
-    ["##teamcity[testFinished name=\'title3\' duration=\'123\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'test3\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteStarted name=\'test4\' flowId=\'12345\']"],
-    ["##teamcity[testStarted name=\'title4\' flowId=\'12345\']"],
-    ["##teamcity[testFailed name=\'title4\' flowId=\'12345\']"],
-    ["##teamcity[testFinished name=\'title4\' duration=\'123\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'test4\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'to\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'path2\' flowId=\'12345\']"],
-    ["##teamcity[testSuiteFinished name=\'foo/__tests__/file2.js\' flowId=\'12345\']"]
+  ["##teamcity[testSuiteStarted name='foo/__tests__/file.js' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='path' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='to' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test1' flowId='12345']"],
+  ["##teamcity[testStarted name='title1' flowId='12345']"],
+  ["##teamcity[testFailed name='title1' flowId='12345']"],
+  ["##teamcity[testFinished name='title1' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test1' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test2' flowId='12345']"],
+  ["##teamcity[testStarted name='title2' flowId='12345']"],
+  ["##teamcity[testIgnored name='title2' message='pending' flowId='12345']"],
+  ["##teamcity[testFinished name='title2' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test2' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='to' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='path' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='foo/__tests__/file.js' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='foo/__tests__/file2.js' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='path2' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='to' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test3' flowId='12345']"],
+  ["##teamcity[testStarted name='title3' flowId='12345']"],
+  ["##teamcity[testFinished name='title3' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test3' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test4' flowId='12345']"],
+  ["##teamcity[testStarted name='title4' flowId='12345']"],
+  ["##teamcity[testFailed name='title4' flowId='12345']"],
+  ["##teamcity[testFinished name='title4' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test4' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='to' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='path2' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='foo/__tests__/file2.js' flowId='12345']"]
 ];
 
-describe('jest-teamcity', () => {
-    describe('formatter', () => {
-        let consoleFn = console.log;
-        let formatterFn = formatter.log;
+describe("jest-teamcity", () => {
+  describe("formatter", () => {
+    let consoleFn = console.log;
+    let formatterFn = formatter.log;
 
-        beforeAll(() => {
-            console.log = jest.fn().mockImplementation(s => s);
-            const formatterMock = path.sep == '/'
-                ? s => formatterFn(s)
-                : s => formatterFn(s.replace(/\\/g, '/'));
-            formatter.log = jest.fn().mockImplementation(formatterMock);
-        });
-
-        beforeEach(() => {
-            console.log.mockReset();
-            formatter.log.mockClear();
-        });
-
-        afterAll(() => {
-            console.log = consoleFn;
-            formatter.log = formatterFn;
-        });
-
-        describe('escape()', () => {
-            test('empty string', () => {
-                expect(formatter.escape('')).toEqual('');
-                expect(formatter.escape(null)).toEqual('');
-                expect(formatter.escape(undefined)).toEqual('');
-            });
-
-            test('escape', () => {
-                expect(formatter.escape(`|test[test2]|
-test3`)).toEqual('||test|[test2|]|||ntest3');
-            });
-        });
-
-        describe('printTestLog', () => {
-            test('empty tests', () => {
-                ['', null, undefined, {}, [], 0].forEach((data) => {
-                    formatter.printTestLog(data);
-                    expect(console.log.mock.calls).toHaveLength(0);
-                });
-            });
-
-            test('with data', () => {
-                formatter.printTestLog(formatter.collectSuites(testData, '/Users/test'), '12345');
-                expect(console.log.mock.calls).toEqual(consoleOutput);
-            });
-        });
-
-        test('log', () => {
-            formatter.log('test');
-            expect(console.log.mock.calls[0][0]).toEqual('test');
-        });
-
-        describe('collectSuites', () => {
-            test('empty', () => {
-                expect(formatter.collectSuites()).toEqual({});
-                expect(formatter.collectSuites(null)).toEqual({});
-                expect(formatter.collectSuites(undefined)).toEqual({});
-                expect(formatter.collectSuites('')).toEqual({});
-                expect(formatter.collectSuites([], '/')).toEqual({});
-            });
-
-            test('with result', () => {
-                const fileKey = ['foo', '__tests__', 'file.js'].join(path.sep);
-                const file2Key = ['foo', '__tests__', 'file2.js'].join(path.sep);
-                expect(formatter.collectSuites(testData, '/Users/test')).toEqual({
-                    [fileKey]: {
-                        path: expect.objectContaining({
-                            to: expect.objectContaining({
-                                test1: expect.any(Object),
-                                test2: expect.any(Object),
-                            })
-                        }),
-                    },
-                    [file2Key]: {
-                        path2: expect.objectContaining({
-                            to: expect.objectContaining({
-                                test3: expect.any(Object),
-                                test4: expect.any(Object),
-                            })
-                        })
-                    }
-                });
-            });
-        });
-
-        test('formatReport', () => {
-            formatter.formatReport(testData, '/Users/test/', '12345');
-            expect(console.log.mock.calls).toEqual(consoleOutput);
-        });
+    beforeAll(() => {
+      console.log = jest.fn().mockImplementation(s => s);
+      const formatterMock = path.sep == "/" ? s => formatterFn(s) : s => formatterFn(s.replace(/\\/g, "/"));
+      formatter.log = jest.fn().mockImplementation(formatterMock);
     });
+
+    beforeEach(() => {
+      console.log.mockReset();
+      formatter.log.mockClear();
+    });
+
+    afterAll(() => {
+      console.log = consoleFn;
+      formatter.log = formatterFn;
+    });
+
+    describe("escape()", () => {
+      test("empty string", () => {
+        expect(formatter.escape("")).toEqual("");
+        expect(formatter.escape(null)).toEqual("");
+        expect(formatter.escape(undefined)).toEqual("");
+      });
+
+      test("escape", () => {
+        expect(
+          formatter.escape(`|test[test2]|
+test3`)
+        ).toEqual("||test|[test2|]|||ntest3");
+      });
+    });
+
+    describe("printTestLog", () => {
+      test("empty tests", () => {
+        ["", null, undefined, {}, [], 0].forEach(data => {
+          formatter.printTestLog(data);
+          expect(console.log.mock.calls).toHaveLength(0);
+        });
+      });
+
+      test("with data", () => {
+        formatter.printTestLog(formatter.collectSuites(testData, "/Users/test"), "12345");
+        expect(console.log.mock.calls).toEqual(consoleOutput);
+      });
+    });
+
+    test("log", () => {
+      formatter.log("test");
+      expect(console.log.mock.calls[0][0]).toEqual("test");
+    });
+
+    describe("collectSuites", () => {
+      test("empty", () => {
+        expect(formatter.collectSuites()).toEqual({});
+        expect(formatter.collectSuites(null)).toEqual({});
+        expect(formatter.collectSuites(undefined)).toEqual({});
+        expect(formatter.collectSuites("")).toEqual({});
+        expect(formatter.collectSuites([], "/")).toEqual({});
+      });
+
+      test("with result", () => {
+        const fileKey = ["foo", "__tests__", "file.js"].join(path.sep);
+        const file2Key = ["foo", "__tests__", "file2.js"].join(path.sep);
+        expect(formatter.collectSuites(testData, "/Users/test")).toEqual({
+          [fileKey]: {
+            path: expect.objectContaining({
+              to: expect.objectContaining({
+                test1: expect.any(Object),
+                test2: expect.any(Object)
+              })
+            })
+          },
+          [file2Key]: {
+            path2: expect.objectContaining({
+              to: expect.objectContaining({
+                test3: expect.any(Object),
+                test4: expect.any(Object)
+              })
+            })
+          }
+        });
+      });
+    });
+
+    test("formatReport", () => {
+      formatter.formatReport(testData, "/Users/test/", "12345");
+      expect(console.log.mock.calls).toEqual(consoleOutput);
+    });
+  });
 });
--- __tests__/index.js
+++ __tests__/index.js
@@ -1,68 +1,70 @@
-'use strict';
-
 const consoleOutput = [
-    ["##teamcity[testSuiteStarted name=\'foo/__tests__/file.js\']"],
-    ["##teamcity[testSuiteStarted name=\'path\']"],
-    ["##teamcity[testSuiteStarted name=\'to\']"],
-    ["##teamcity[testSuiteStarted name=\'test1\']"],
-    ["##teamcity[testStarted name=\'title1\']"],
-    ["##teamcity[testFailed name=\'title1\']"],
-    ["##teamcity[testFinished name=\'title1\' duration=\'123\']"],
-    ["##teamcity[testSuiteFinished name=\'test1\']"],
-    ["##teamcity[testSuiteStarted name=\'test2\']"],
-    ["##teamcity[testStarted name=\'title2\']"],
-    ["##teamcity[testIgnored name=\'title2\' message=\'pending\']"],
-    ["##teamcity[testFinished name=\'title2\' duration=\'123\']"],
-    ["##teamcity[testSuiteFinished name=\'test2\']"],
-    ["##teamcity[testSuiteFinished name=\'to\']"],
-    ["##teamcity[testSuiteFinished name=\'path\']"],
-    ["##teamcity[testSuiteFinished name=\'foo/__tests__/file.js\']"],
-    ["##teamcity[testSuiteStarted name=\'foo/__tests__/file2.js\']"],
-    ["##teamcity[testSuiteStarted name=\'path2\']"],
-    ["##teamcity[testSuiteStarted name=\'to\']"],
-    ["##teamcity[testSuiteStarted name=\'test3\']"],
-    ["##teamcity[testStarted name=\'title3\']"],
-    ["##teamcity[testFinished name=\'title3\' duration=\'123\']"],
-    ["##teamcity[testSuiteFinished name=\'test3\']"],
-    ["##teamcity[testSuiteStarted name=\'test4\']"],
-    ["##teamcity[testStarted name=\'title4\']"],
-    ["##teamcity[testFailed name=\'title4\']"],
-    ["##teamcity[testFinished name=\'title4\' duration=\'123\']"],
-    ["##teamcity[testSuiteFinished name=\'test4\']"],
-    ["##teamcity[testSuiteFinished name=\'to\']"],
-    ["##teamcity[testSuiteFinished name=\'path2\']"],
-    ["##teamcity[testSuiteFinished name=\'foo/__tests__/file2.js\']"]
+  ["##teamcity[testSuiteStarted name='foo/__tests__/file.js' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='path' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='to' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test1' flowId='12345']"],
+  ["##teamcity[testStarted name='title1' flowId='12345']"],
+  ["##teamcity[testFailed name='title1' flowId='12345']"],
+  ["##teamcity[testFinished name='title1' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test1' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test2' flowId='12345']"],
+  ["##teamcity[testStarted name='title2' flowId='12345']"],
+  ["##teamcity[testIgnored name='title2' message='pending' flowId='12345']"],
+  ["##teamcity[testFinished name='title2' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test2' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='to' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='path' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='foo/__tests__/file.js' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='foo/__tests__/file2.js' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='path2' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='to' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test3' flowId='12345']"],
+  ["##teamcity[testStarted name='title3' flowId='12345']"],
+  ["##teamcity[testFinished name='title3' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test3' flowId='12345']"],
+  ["##teamcity[testSuiteStarted name='test4' flowId='12345']"],
+  ["##teamcity[testStarted name='title4' flowId='12345']"],
+  ["##teamcity[testFailed name='title4' flowId='12345']"],
+  ["##teamcity[testFinished name='title4' duration='123' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='test4' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='to' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='path2' flowId='12345']"],
+  ["##teamcity[testSuiteFinished name='foo/__tests__/file2.js' flowId='12345']"]
 ];
-const testData = require('./data');
-const reporter = require('../lib/index');
+const testData = require("./data");
+const reporter = require("../lib/index");
 
-describe('jest-teamcity', () => {
-    beforeAll(() => {
-        console.log = jest.fn().mockImplementation(s => s);
+describe("jest-teamcity", () => {
+  beforeAll(() => {
+    process.env.TEAMCITY_FLOWID = 12345;
+    console.log = jest.fn().mockImplementation(s => s);
+  });
+
+  beforeEach(() => {
+    console.log.mockReset();
+  });
+
+  afterAll(() => {
+    console.log = consoleFn;
+  });
+
+  describe("module", () => {
+    it("enabled (with TEAMCITY_VERSION)", () => {
+      process.env.TEAMCITY_VERSION = "0.0.0";
+
+      const originalCwd = process.cwd();
+      process.cwd = function() {
+        return "/Users/test";
+      };
+      reporter({ testResults: testData });
+      expect(console.log.mock.calls).toEqual(consoleOutput);
+      process.cwd = originalCwd;
     });
 
-    beforeEach(() => {
-        console.log.mockReset();
+    it("disabled", () => {
+      delete process.env.TEAMCITY_VERSION;
+      const result = reporter({ test: 1 });
+      expect(result).toEqual({ test: 1 });
     });
-
-    afterAll(() => {
-        console.log = consoleFn;
-    });
-
-    describe('module', () => {
-        test('enabled (with TEAMCITY_VERSION)', () => {
-            process.env.TEAMCITY_VERSION = '0.0.0';
-            const originalCwd = process.cwd();
-            process.cwd = function () { return '/Users/test'; };
-            reporter({ testResults: testData });
-            expect(console.log.mock.calls).toEqual(consoleOutput);
-            process.cwd = originalCwd;
-        });
-
-        test('disabled', () => {
-            delete process.env.TEAMCITY_VERSION;
-            const result = reporter({ test: 1 });
-            expect(result).toEqual({ test: 1 });
-        });
-    });
+  });
 });
